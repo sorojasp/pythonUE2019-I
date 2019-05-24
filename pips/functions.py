f3391259c177a486ce723b0ed19c885de3dadd3d
sorojasp <stivenorlandorojaspulido@gmail.com>
2019-05-24 11:58:18 -0500

    clase de programacion estructurada - 24/06/2019

---
+++ pips/functions.py
@@ -0,0 +1,7 @@
+def imprimeNumeros():
+    i=0
+    while i<3:
+        print(i)
+        i=i+1
+    return ("hola", "marte")
+    
---
+++ pips/pipExplanation.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import Tkinter
+##import tensorflow as tf
+import camelcase
+from functions import imprimeNumeros
+
+
+esterExposito=imprimeNumeros()
+
+
+print("valor variable esterEXposito")
+print(esterExposito)
+
+
+
+
+datoPrueba = [ { "holiday": 89 }]
+
+## mensaje = tf.string_join (["hola", "mundo"])
